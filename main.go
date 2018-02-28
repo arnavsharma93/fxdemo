8124f48a15365a8ae6a2ae46ab16240c426bb04e
Arnav Sharma <arnav@uber.com>
2018-02-28 09:28:38 +0530

    Providing a new http handler using fx.Provide

--- main.go
+++ main.go
@@ -1,11 +1,33 @@
 package main
 
 import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+
 	"go.uber.org/fx"
 )
 
 func main() {
-	app := fx.New()
+	app := fx.New(
+		fx.Provide(NewHandler),
+	)
 	app.Run()
 
+}
+
+// NewHandler function instantiates our handler
+func NewHandler() http.Handler {
+	fmt.Println("Constructing a new http handler")
+
+	handler := func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("Handler was called")
+		_, err := io.WriteString(w, "hello world\n")
+		if err != nil {
+			log.Println("err, please do something ", err)
+		}
+	}
+
+	return http.HandlerFunc(handler)
 }
